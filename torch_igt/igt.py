8681dda1bd488da31bb5a1711f95b91caf4ee508
seba-1511 <seba-1511@hotmail.com>
2018-07-22 02:24:51 -0400

    Fixed import typo

--- torch_igt/igt.py
+++ torch_igt/igt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import torch as th
-from torch.optim.Optimizer import Optimizer, required
+from torch.optim.optimizer import Optimizer, required
 
 
 class MomentumIGT(Optimizer):
